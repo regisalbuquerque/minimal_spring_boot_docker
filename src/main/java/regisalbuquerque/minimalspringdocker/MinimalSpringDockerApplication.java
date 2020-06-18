0a6874a270baead9f61ba9279a54cd81da77a226
Regis Albuquerque <regis.albuquerque1@gmail.com>
2020-06-18 16:26:08 -0500

    Adding main '/' endpoint

--- src/main/java/regisalbuquerque/minimalspringdocker/MinimalSpringDockerApplication.java
+++ src/main/java/regisalbuquerque/minimalspringdocker/MinimalSpringDockerApplication.java
@@ -2,12 +2,20 @@ package regisalbuquerque.minimalspringdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MinimalSpringDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinimalSpringDockerApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
 	}
 
 }
