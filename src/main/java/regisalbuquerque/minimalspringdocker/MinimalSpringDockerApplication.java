5d9d622d4ed800714b2fe64b1231c8c201ea4176
Regis Albuquerque <regis.albuquerque1@gmail.com>
2020-06-18 16:17:31 -0500

    Minimal Spring Boot Project with Docker generated by Spring Initializr website tool

---
+++ src/main/java/regisalbuquerque/minimalspringdocker/MinimalSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package regisalbuquerque.minimalspringdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalSpringDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/regisalbuquerque/minimalspringdocker/MinimalSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package regisalbuquerque.minimalspringdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalSpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
